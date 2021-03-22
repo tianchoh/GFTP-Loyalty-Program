b807a970b57e2e477554908de957b213ecd0f632
tianchoh <tianchoh@users.noreply.github.com>
2021-03-22 22:16:03 +0800

    Add files via upload

---
+++ GFTP-loyalty-program/web-app/enrollAdmin.js
@@ -0,0 +1,67 @@
+'use strict';
+
+const FabricCAServices = require('fabric-ca-client');
+//const { FileSystemWallet, X509WalletMixin } = require('fabric-network');
+const { Wallets } = require('fabric-network');
+const fs = require('fs');
+const path = require('path');
+
+// capture network variables from config.json
+const configPath = path.join(process.cwd(), 'config.json');
+const configJSON = fs.readFileSync(configPath, 'utf8');
+const config = JSON.parse(configJSON);
+let connection_file = config.connection_file;
+let appAdmin = config.appAdmin;
+let appAdminSecret = config.appAdminSecret;
+let orgMSPID = config.orgMSPID;
+let caName = config.caName;
+
+const ccpPath = path.join(process.cwd(), connection_file);
+const ccpJSON = fs.readFileSync(ccpPath, 'utf8');
+const ccp = JSON.parse(ccpJSON);
+
+
+async function main() {
+    try {
+
+        // Create a new CA client for interacting with the CA.
+        const caURL = ccp.certificateAuthorities[caName].url;
+        const ca = new FabricCAServices(caURL);
+        
+        // Create a new file system based wallet for managing identities.
+        const walletPath = path.join(process.cwd(), 'wallet');
+        //const wallet = new FileSystemWallet(walletPath);
+        const wallet = await Wallets.newFileSystemWallet(walletPath);
+        console.log(`Wallet path: ${walletPath}`);
+
+        // Check to see if we've already enrolled the admin user.
+        //const adminExists = await wallet.exists(appAdmin);
+        const adminExists = await wallet.get(appAdmin);
+        if (adminExists) {
+            console.log('An identity for the admin user "admin" already exists in the wallet');
+            return;
+        }
+        
+        // Enroll the admin user, and import the new identity into the wallet.
+        const enrollment = await ca.enroll({ enrollmentID: appAdmin, enrollmentSecret: appAdminSecret });
+        //const identity = X509WalletMixin.createIdentity(orgMSPID, enrollment.certificate, enrollment.key.toBytes());
+        
+        const x509Identity = {
+            credentials: {
+                certificate: enrollment.certificate,
+                privateKey: enrollment.key.toBytes(),
+            },
+            mspId: 'Org1MSP',
+            type: 'X.509',
+        };
+        console.log('clear');
+        wallet.put(appAdmin, x509Identity);
+        console.log('msg: Successfully enrolled admin user ' + appAdmin + ' and imported it into the wallet');
+
+    } catch (error) {
+        console.error('Failed to enroll admin user ' + appAdmin + ': ' + error);
+        process.exit(1);
+    }
+}
+
+main();
